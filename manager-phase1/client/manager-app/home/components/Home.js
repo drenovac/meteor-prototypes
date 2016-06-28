1357f289ce9adf4456fafb0459cb95dfa672069e
Floyd Price <f.price@floydprice.com>
2016-06-28 01:10:41 +0100

    changed to a roster view

--- manager-phase1/client/manager-app/home/components/Home.js
+++ manager-phase1/client/manager-app/home/components/Home.js
@@ -1,8 +1,9 @@
 import React from 'react'
 import CalendarView from '../containers/CalendarView'
+import ShiftView from '../containers/ShiftView'
 
 export default (props) => {
   return (
-    <CalendarView />
+    <ShiftView />
   )
 }
